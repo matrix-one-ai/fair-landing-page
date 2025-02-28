c12efe93d6ea76ba671ac29c425815326e343a4a
devart002 <devart002@gmail.com>
2025-02-28 09:49:41 -0500

    feat: implement meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "SAMI",
-  description: "SAMI - Matrix Agent",
+  title: "FAIR Frameworks for (human) and AI runtime",
+  description: "Deploy, launch and automate and crypto team",
 };
 
 export default function RootLayout({
